27c8ff750a54be2b68074d1a768b4a26b0104045
zxenlogics <denzilnbrown@gmail.com>
2013-03-12 13:43:38 -0400

    Cleaned up log4net references

--- eSamples.Domain/ServiceAgentBase.cs
+++ eSamples.Domain/ServiceAgentBase.cs
@@ -4,13 +4,12 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using log4net;
 
 namespace eSamples.Domain
 {
     public abstract class ServiceAgentBase
     {
-        private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+       // private static readonly ILog Log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
         protected void OnServiceFailure()
         {

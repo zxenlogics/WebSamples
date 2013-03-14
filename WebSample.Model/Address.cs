837c608ef9505bc54918c90ee694df5cfdd28f31
zxenlogics <denzilnbrown@gmail.com>
2013-03-14 15:14:53 -0400

    Resolve references, added EntityFramework to Models project via Nuget

---
+++ WebSample.Model/Address.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eSamples.Domain
+{
+    [Table("Address")]
+    public class Address
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int AddressId { get; set; }
+        public string HcpId { get; set; }
+        public string OrganizationName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zip { get; set; }
+        public string Phone { get; set; }
+    }
+}

0571f83030182f284e21a8442f5f23aa8662c69e
zxenlogics <denzilnbrown@gmail.com>
2013-03-16 17:55:41 -0400

    Misc clean up of product type and instances

---
+++ WebSample.Model/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebSample.Model
+{
+    public class Product
+    {
+        public enum ProductType
+        {
+            Sample,
+            OTC,
+            Regulated,
+        }
+
+        [Key]
+        public string ProductID { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double UnitPrice { get; set; }
+        public string Img { get; set; }
+        public ProductType Type { get; set; }
+
+    }
+}

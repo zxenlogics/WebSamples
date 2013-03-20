ae20472984aa7775a4a990894a0236a4a87fe7d7
zxenlogics <denzilnbrown@gmail.com>
2013-03-20 18:51:58 -0400

    Changed layout view and css and added product pages

---
+++ WebSample.Web/Controllers/ProductsController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using WebSample.Model;
+using WebSample.Domain.Contracts;
+using WebSample.Domain.Repositories;
+
+namespace WebSample.Web.Controllers
+{
+    public class ProductsController : Controller
+    {
+        IProductRepository db;
+
+        public ProductsController()
+        {
+            db = new ProductRepository();
+        }
+
+        public ActionResult ViewAll()
+        {
+            IEnumerable<Product> products = db.GetProducts();
+
+            return View(products);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Authorize]
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+    }
+}
